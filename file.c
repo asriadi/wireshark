asriadi/wireshark

commit 0431165301615e08316a98904cdf2f672585d48d

Inline a few checks in the add_packet_to_packet_list() function since it's called in tight loop. This will help us to avoid calling functions that simply returns. This is especially the case when we're dealing with the new packet list due to its dissect-on-demand nature where colour/column construction is delayed until the row becomes visible.
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@29435 f5534014-38df-0310-8fa8-9805f1628bb7

file.c
@@ -1090,9 +1090,13 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
   col_custom_prime_edt(&edt, cinfo);
 #endif
 
-  tap_queue_init(&edt);
+  if (filtering_tap_listeners)
+    tap_queue_init(&edt);
+
   epan_dissect_run(&edt, pseudo_header, buf, fdata, cinfo);
-  tap_push_tapped_queue(&edt);
+
+  if (filtering_tap_listeners)
+    tap_push_tapped_queue(&edt);
 
   /* If we have a display filter, apply it if we're refiltering, otherwise
      leave the "passed_dfilter" flag alone.
@@ -1107,7 +1111,8 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
 
 #ifdef NEW_PACKET_LIST
     if (add_to_packet_list) {
-        epan_dissect_fill_in_columns(&edt, FALSE);
+        if (cinfo)
+            epan_dissect_fill_in_columns(&edt, FALSE);
         row = new_packet_list_append(cinfo, fdata, &edt.pi);
     }
 #endif

commit 2cccb7f300935591c7090ab95e97895b50874632

We shouldn't conditionalise on _filtering_ tap listerners to determine whether tapping should be performed or not. We also need to handle _normal_ tap listerners. This change backs out the regression introduced in r29435.
Thanks to Didier: https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=3916


git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@29475 f5534014-38df-0310-8fa8-9805f1628bb7

file.c
@@ -1090,13 +1090,9 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
   col_custom_prime_edt(&edt, cinfo);
 #endif
 
-  if (filtering_tap_listeners)
-    tap_queue_init(&edt);
-
+  tap_queue_init(&edt);
   epan_dissect_run(&edt, pseudo_header, buf, fdata, cinfo);
-
-  if (filtering_tap_listeners)
-    tap_push_tapped_queue(&edt);
+  tap_push_tapped_queue(&edt);
 
   /* If we have a display filter, apply it if we're refiltering, otherwise
      leave the "passed_dfilter" flag alone.