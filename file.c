asriadi/wireshark

commit b96d4e4d2bcb18415e554589c0ca4d5081f69a83

Move 'Delta timestamp to previous captured frame' calculation inside add_packet_to_packet_list(). This is done in order to streamline add_packet_to_packet_list() with frame_data_init(). Hopefully we can switch to frame_data_init() soon
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@30052 f5534014-38df-0310-8fa8-9805f1628bb7

file.c
@@ -77,9 +77,11 @@
 gboolean auto_scroll_live;
 #endif
 
+static guint32 cum_bytes;
 static nstime_t first_ts;
 static nstime_t prev_dis_ts;
-static guint32 cum_bytes;
+static nstime_t prev_cap_ts;
+
 static gulong computed_elapsed;
 
 static void cf_reset_state(capture_file *cf);
@@ -321,6 +323,7 @@ cf_open(capture_file *cf, const char *fname, gboolean is_tempfile, int *err)
   nstime_set_zero(&cf->elapsed_time);
   nstime_set_unset(&first_ts);
   nstime_set_unset(&prev_dis_ts);
+  nstime_set_unset(&prev_cap_ts);
   cum_bytes = 0;
 
 #if GLIB_CHECK_VERSION(2,10,0)
@@ -1103,6 +1106,14 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
     prev_dis_ts = fdata->abs_ts;
   }
 
+  /* If we don't have the time stamp of the previous captured packet,
+     it's because this is the first packet.  Save the time
+     stamp of this packet as the time stamp of the previous captured
+     packet. */
+  if (nstime_is_unset(&prev_cap_ts)) {
+    prev_cap_ts = fdata->abs_ts;
+  }
+
   /* Get the time elapsed between the first packet and this packet. */
   nstime_delta(&fdata->rel_ts, &fdata->abs_ts, &first_ts);
 
@@ -1117,6 +1128,11 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
   /* Get the time elapsed between the previous displayed packet and
      this packet. */
   nstime_delta(&fdata->del_dis_ts, &fdata->abs_ts, &prev_dis_ts);
+
+  /* Get the time elapsed between the previous captured packet and
+     this packet. */
+  nstime_delta(&fdata->del_cap_ts, &fdata->abs_ts, &prev_cap_ts);
+  prev_cap_ts = fdata->abs_ts;
 
   /* If either
     + we have a display filter and are re-applying it;
@@ -1408,11 +1424,6 @@ read_packet(capture_file *cf, dfilter_t *dfcode,
   fdata->col_text_len = se_alloc0(sizeof(fdata->col_text_len) * (cf->cinfo.num_cols));
   fdata->col_text = se_alloc0(sizeof(fdata->col_text) * (cf->cinfo.num_cols));
 #endif
-
-  if (cf->plist_end != NULL)
-    nstime_delta(&fdata->del_cap_ts, &fdata->abs_ts, &cf->plist_end->abs_ts);
-  else
-    nstime_set_zero(&fdata->del_cap_ts);
 
   passed = TRUE;
   if (cf->rfcode) {
@@ -1885,6 +1896,7 @@ rescan_packets(capture_file *cf, const char *action, const char *action_item,
      the display list. */
   nstime_set_unset(&first_ts);
   nstime_set_unset(&prev_dis_ts);
+  nstime_set_unset(&prev_cap_ts);
   cum_bytes = 0;
 
   /* Update the progress bar when it gets to this value. */
@@ -2410,6 +2422,7 @@ ref_time_packets(capture_file *cf)
 
   nstime_set_unset(&first_ts);
   nstime_set_unset(&prev_dis_ts);
+  nstime_set_unset(&prev_cap_ts);
   cum_bytes = 0;
 
   for (fdata = cf->plist_start; fdata != NULL; fdata = fdata->next) {

rawshark.c
@@ -121,7 +121,7 @@
  */
 static const gchar decode_as_arg_template[] = "<layer_type>==<selector>,<decode_as_protocol>";
 
-static guint32 cum_bytes = 0;
+static guint32 cum_bytes;
 static nstime_t first_ts;
 static nstime_t prev_dis_ts;
 static nstime_t prev_cap_ts;

commit 059500a4bb784ca4e360bba389e2aae76153e544

Fixed 'Delta timestamp to previous captured frame' calculation in old packet list.  This bug was introduced in r30052.
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@30758 f5534014-38df-0310-8fa8-9805f1628bb7

file.c
@@ -1178,40 +1178,8 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
   /* just add some value here until we know if it is being displayed or not */
   fdata->cum_bytes  = cum_bytes + fdata->pkt_len;
 
-  /* If we don't have the time stamp of the first packet in the
-     capture, it's because this is the first packet.  Save the time
-     stamp of this packet as the time stamp of the first packet. */
-  if (nstime_is_unset(&first_ts)) {
-    first_ts  = fdata->abs_ts;
-  }
-  /* if this frames is marked as a reference time frame, reset
-     firstsec and firstusec to this frame */
-  if(fdata->flags.ref_time){
-    first_ts = fdata->abs_ts;
-  }
-
-  /* If we don't have the time stamp of the previous displayed packet,
-     it's because this is the first displayed packet.  Save the time
-     stamp of this packet as the time stamp of the previous displayed
-     packet. */
-  if (nstime_is_unset(&prev_dis_ts)) {
-    prev_dis_ts = fdata->abs_ts;
-  }
-
-  /* Get the time elapsed between the first packet and this packet. */
-  nstime_delta(&fdata->rel_ts, &fdata->abs_ts, &first_ts);
-
-  /* If it's greater than the current elapsed time, set the elapsed time
-     to it (we check for "greater than" so as not to be confused by
-     time moving backwards). */
-  if ((gint32)cf->elapsed_time.secs < fdata->rel_ts.secs
-  || ((gint32)cf->elapsed_time.secs == fdata->rel_ts.secs && (gint32)cf->elapsed_time.nsecs < fdata->rel_ts.nsecs)) {
-    cf->elapsed_time = fdata->rel_ts;
-  }
-
-  /* Get the time elapsed between the previous displayed packet and
-     this packet. */
-  nstime_delta(&fdata->del_dis_ts, &fdata->abs_ts, &prev_dis_ts);
+  frame_data_set_before_dissect(fdata, &cf->elapsed_time,
+                                &first_ts, &prev_dis_ts, &prev_cap_ts);
 
   /* If either
 
@@ -1262,17 +1230,7 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
 
   if( (fdata->flags.passed_dfilter) || (fdata->flags.ref_time) )
   {
-    /* This frame either passed the display filter list or is marked as
-       a time reference frame.  All time reference frames are displayed
-       even if they dont pass the display filter */
-    if(fdata->flags.ref_time){
-      /* if this was a TIME REF frame we should reset the cul bytes field */
-      cum_bytes = fdata->pkt_len;
-      fdata->cum_bytes =  cum_bytes;
-    } else {
-      /* increase cum_bytes with this packets length */
-      cum_bytes += fdata->pkt_len;
-    }
+    frame_data_set_after_dissect(fdata, &cum_bytes, &prev_dis_ts);
 
     epan_dissect_fill_in_columns(&edt, FALSE, TRUE);
 
@@ -1306,10 +1264,6 @@ add_packet_to_packet_list(frame_data *fdata, capture_file *cf,
      if (fdata->flags.marked) {
        packet_list_set_colors(row, &prefs.gui_marked_fg, &prefs.gui_marked_bg);
      }
-
-    /* Set the time of the previous displayed frame to the time of this
-       frame. */
-    prev_dis_ts = fdata->abs_ts;
 
     cf->displayed_count++;
   }
@@ -2121,6 +2075,7 @@ rescan_packets(capture_file *cf, const char *action, const char *action_item,
      the display list. */
   nstime_set_unset(&first_ts);
   nstime_set_unset(&prev_dis_ts);
+  nstime_set_unset(&prev_cap_ts);
   cum_bytes = 0;
 
   /* Update the progress bar when it gets to this value. */