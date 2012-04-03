asriadi/wireshark

commit f0dfc598d76a77e392939edbce5b1038d22962cc

Replace use of tvb_get_ephemeral_faked_unicode() by use of tvb_get_ephemeral_unicode_string(); Fix encoding arg.
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@39531 f5534014-38df-0310-8fa8-9805f1628bb7

asn1/x509sat/x509sat.cnf
@@ -356,7 +356,7 @@ XDayOf/fifth				fifth_dayof
 	if (! wide_tvb) {
 		return offset;
 	}
-	string = tvb_get_ephemeral_faked_unicode (wide_tvb, 0, tvb_length(wide_tvb) / 2, FALSE);
+	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb) / 2, ENC_BIG_ENDIAN);
 	proto_item_append_text(actx->created_item, " %s", string);	
 
 #.END

epan/dissectors/packet-x509sat.c
@@ -1518,7 +1518,7 @@ dissect_x509sat_SyntaxBMPString(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, in
 	if (! wide_tvb) {
 		return offset;
 	}
-	string = tvb_get_ephemeral_faked_unicode (wide_tvb, 0, tvb_length(wide_tvb) / 2, FALSE);
+	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb) / 2, ENC_BIG_ENDIAN);
 	proto_item_append_text(actx->created_item, " %s", string);	
 
 

commit 45483b05a8c23160b182635a2b3b57caeb2eceef

Fix off by one shift error introduced in r39531.
In tvb_get_ephemeral_unicode_string() length is number of bytes (not characters).


git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@41903 f5534014-38df-0310-8fa8-9805f1628bb7

asn1/x509sat/x509sat.cnf
@@ -356,7 +356,7 @@ XDayOf/fifth				fifth_dayof
 	if (! wide_tvb) {
 		return offset;
 	}
-	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb) / 2, ENC_BIG_ENDIAN);
+	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb), ENC_BIG_ENDIAN);
 	proto_item_append_text(actx->created_item, " %s", string);	
 
 #.END

epan/dissectors/packet-x509sat.c
@@ -1518,7 +1518,7 @@ dissect_x509sat_SyntaxBMPString(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, in
 	if (! wide_tvb) {
 		return offset;
 	}
-	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb) / 2, ENC_BIG_ENDIAN);
+	string = tvb_get_ephemeral_unicode_string (wide_tvb, 0, tvb_length(wide_tvb), ENC_BIG_ENDIAN);
 	proto_item_append_text(actx->created_item, " %s", string);	
 
 